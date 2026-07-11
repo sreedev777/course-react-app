0098bc5c9c9c3ff7ccaa6f438f5e84f6039c906d
sreedev777 <sreedevkailas@gmail.com>
2026-07-11 15:10:13 +0530

    view commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import InsertCourse from './components/InsertCourse'
+import ViewCourse from './components/ViewCourse'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <InsertCourse/>
+      <ViewCourse/>
     </>
   )
 }
